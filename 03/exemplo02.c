8cbb276e4f5d553e6a06928035c191f225ef0634
Alessandro <alessandro.valerio.dias@gmail.com>
2020-10-08 20:36:07 -0300

    new examples

--- 03/exemplo02.c
+++ 03/exemplo02.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
+// valor é uma Variavel Global
+int valor = 10;
+
+// numero é um Parametro formal
 int parOuImpar(int numero){
+
+    numero = numero + 10;
+    printf("numero = %d\n", numero);
+
     if (numero % 2 == 0)
         return 0;
     else 
@@ -8,9 +16,14 @@ int parOuImpar(int numero){
 }
 
 int main() {
+
+    // valor Informado é uma variavel local
     int valorInformado;
+
     printf("\ninforme um valor: ");
     scanf("%i", &valorInformado);
+
+    printf("valor global = %d\n", valor);
 
     int resultado = parOuImpar(valorInformado);
     if (resultado==0){
@@ -18,5 +31,8 @@ int main() {
     } else {
         printf("O numero eh impar");
     }   
+
+    printf("valor informado = %d\n", valorInformado);
+
 
 }
